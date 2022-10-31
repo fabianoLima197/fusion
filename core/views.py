9d2815e6edca6e1cbe39dfe18cc7edfaee9b4cd9
Fabiano <fabianolimaads@gmail.com>
2022-10-31 14:58:42 -0300

    conclusao da aula 52... muito bom tudo deu certo

--- core/views.py
+++ core/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
+
+from .models import Servico, Funcionario
+from .forms import ContatoForm
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_email()
+        messages.success(self.request, 'E-mail enviado com sucesso!')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro no envio! --Não Enviado--')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
+
+
+
 
--- fusion/settings.py
+++ fusion/settings.py
@@ -133,6 +133,18 @@ MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Email teste console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+'''
+# Email produção
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = 'no-reply@fusion.com.br'
+EMAIL_PORT = '587'
+EMAIL_USE_TSL = TRUE => se tiver segurança no e-mail
+EMAIL_HOST_FROM_EMAIL = 'contato@fusion.com.br => email da empresa que vai enviar '
+'''
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
