bc7e4b6a67a10a463ccfe1ec44442b83efdd9451
Fabiano <fabianolimaads@gmail.com>
2022-10-31 18:51:48 -0300

    Aulas de testes

---
+++ core/tests/test_models.py
@@ -0,0 +1,35 @@
+import uuid
+from django.test import TestCase
+from model_mommy import mommy
+
+from core.models import get_file_path
+
+class GetFilePathTestCase(TestCase):
+    def setUp(self):
+        self.filename = f'{uuid.uuid4()}.png'
+
+    def test_get_file_path(self):
+        arquivo = get_file_path(None, 'teste.png')
+        self.assertTrue(len(arquivo), len(self.filename))
+
+class ServicoTestCase(TestCase):
+    def setUp(self):
+        self.servicos = mommy.make('Servicos')
+
+    def tes_str(self):
+        self.assertEquals(str(self.servicos), self.servicos.servicos)
+
+class CargoTestCase(TestCase):
+    def setUp(self):
+        self.cargo = mommy.make('Cargo')
+
+    def test_str(self):
+        self.assertEqual(str(self.cargo), self.cargo.cargo)
+
+class FuncionarioTestCase(TestCase):
+    def setUp(self):
+        self.funcionario = mommy.make('Funcionario')
+
+    def test_str(self):
+        self.assertEqual(str(self.funcionario), self.funcionario.nome)
+
